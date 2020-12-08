06d4003512040fffd126036bf5757d6542824c29
Steve Baker <steve.baker@coop.co.uk>
2020-12-08 08:08:17 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aoc_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+Ÿ
